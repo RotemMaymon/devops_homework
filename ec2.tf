8500a981e5553b3bb61cd535e9318316659c7e8c
Rotem Maymon <96170558+RotemMaymon@users.noreply.github.com>
2022-01-13 11:38:21 +0200

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "web1" {
+
+    ami = "${lookup(var.AMI, var.AWS_REGION)}"
+    instance_type = "t2.micro"
+
+    # VPC
+    subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+
+    # Security Group
+    vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]
+
