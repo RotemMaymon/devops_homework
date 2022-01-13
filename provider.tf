eefc87d7a00db1a81cfd6c15b75e3c479c29f2eb
Rotem Maymon <96170558+RotemMaymon@users.noreply.github.com>
2022-01-13 12:11:49 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,19 @@
 provider "aws" {
-    region = "${var.AWS_REGION}"
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      Environment = var.env
+      Project     = "<REPLACE>"
+      Jira_ticket = "<REPLACE>"
+    }
+  }
+}
+terraform {
+  required_version = ">= 1.0.5"
+  required_providers {
+    aws = {
+      version = ">= 3.56.0"
+      source  = "hashicorp/aws"
+    }
+  }
 }
